3750455f374e18c92c1c9c7ed3487febeb26dd43
nayim-hossen <nayimh2017@gmail.com>
2022-11-04 23:46:30 +0600

    add some style and banner image

--- src/componets/HeroBanner.js
+++ src/componets/HeroBanner.js
@@ -1,13 +1,14 @@
 import React from "react";
+import bannerImg from "../image/banner.jpg";
 
 const HeroBanner = () => {
   return (
     <div>
       <div
         className="hero h-96"
-        style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
+        style={{ backgroundImage: `url(${bannerImg})` }}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-overlay bg-[#222] bg-opacity-70"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
